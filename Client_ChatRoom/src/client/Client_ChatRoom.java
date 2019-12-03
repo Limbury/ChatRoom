a2726c7c8976640f6f3aebccc30297f180003548
salourkin <lyijun0738@163.com>
2019-12-03 18:34:14 +0800

    add some code

--- Client_ChatRoom/src/client/Client_ChatRoom.java
+++ Client_ChatRoom/src/client/Client_ChatRoom.java
@@ -7,6 +7,15 @@ import java.net.Socket;
 
 import javax.swing.JOptionPane;
 
+import dataBase.Figures;
+import dataBase.ListInfo;
+import msg.MsgAddFriendResp;
+import msg.MsgChatText;
+import msg.MsgHead;
+import tools.DialogTool;
+import tools.PackageTool;
+import tools.ParseTool;
+
 public class Client_ChatRoom extends Thread {
 	private String serverip;
 	private int port;
@@ -48,6 +57,64 @@ public class Client_ChatRoom extends Thread {
 				System.out.println("与服务器断开连接");
 				JOptionPane.showMessageDialog(null, "与服务器断开连接", "ERROR", JOptionPane.ERROR_MESSAGE);
 				System.exit(0);
+			}
+		}
+	}
+	/**
+	 * sendMsg 发送信息
+	 * 
+	 * @param to
+	 * @param Msg
+	 * @throws IOException
+	 */
+	public void sendMsg(int to, String Msg) throws IOException {
+		MsgChatText mct = new MsgChatText();
+		byte data[] = Msg.getBytes();
+		int TotalLen = 13;
+		TotalLen += data.length;
+		byte type = 0x04;
+		mct.setTotalLen(TotalLen);
+		mct.setType(type);
+		mct.setDest(to);
+		mct.setSrc(OwnJKNum);
+		mct.setMsgText(Msg);
+
+		byte[] sendMsg = PackageTool.packMsg(mct);
+		ous.write(sendMsg);
+		ous.flush();
+	}
+	
+	/**
+	 * processMsg 接受服务器传来的消息
+	 * 
+	 * @throws IOException
+	 */
+	public void processMsg() throws IOException {
+		byte[] data = receiveMsg();
+		// 将数组转换为类
+		MsgHead recMsg = ParseTool.parseMsg(data);
+		byte MsgType = recMsg.getType();
+
+		// 根据不同的信息进行处理
+		if (MsgType == 0x04) {
+			MsgChatText mct = (MsgChatText) recMsg;
+			int from = mct.getSrc();
+			String Msg = mct.getMsgText();
+			DialogTool.ShowMessage(from, Msg);
+		}
+		else if(MsgType == 0x03){//更新好友列表
+			System.out.println("Refresh list");
+			ListInfo list = packlist(recMsg);
+			Figures.list.Refresh_List(list);
+		}
+		if (MsgType == 0x55){
+//			System.out.println("Here");
+			MsgAddFriendResp mafr = (MsgAddFriendResp) recMsg;
+			byte result = mafr.getState();
+			System.out.println("Add Friend Result "+result);
+			if(Figures.afu != null){
+//				System.out.println("To show Result");
+				Figures.afu.showResult(result);
 			}
 		}
 	}
